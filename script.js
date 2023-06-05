c68123cc1a299843bcd965d5c3f70b9ddd0be7ec
Christian Foster <35413427+Fynis96@users.noreply.github.com>
2023-06-05 15:28:14 -0400

    Added cells div for html skeleton, started todo for JS

---
+++ script.js
@@ -0,0 +1,4 @@
+//TODO: Gameboard object that contains array gameboard.  Connects to gameboard div, populates with 3x3 cells that are
+//clickable. each click represents a turn so long as game isnt over and it isn't already filled in.
+//If any winnable condition is met, game ends and winner with the marks representing that condition is output as winner.
+
